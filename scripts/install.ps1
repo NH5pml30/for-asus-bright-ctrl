995b9b2fdc86a6f3aa5fc3bc0e2b680009e24ddd
nh5 <holiavin.nikolai.nh5@gmail.com>
2023-07-23 19:29:38 +0300

    Set the registry key on every user log-in event to handle MyASUS updates

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,4 +1,32 @@
-$action = New-ScheduledTaskAction -Execute "$pwd\for-asus-bright-ctrl.exe" -WorkingDirectory "$pwd"
+# Elevation code from https://superuser.com/a/532109
+
+param(
+    [switch]$elevated,
+    [string]$installDir
+)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        echo "Need admin rights to install!"
+    } else {
+        $action = New-ScheduledTaskAction -Execute "$pwd\for-asus-bright-ctrl.exe" -WorkingDirectory "$pwd"
+        $trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
+        $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+        Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "for-asus-bright-ctrl" -Description "This task runs a third-party program to control ASUS Flicker-Free Dimming with hot keys" -Settings $settings -Force
+        Start-ScheduledTask -TaskName "for-asus-bright-ctrl"
+
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-ExecutionPolicy Bypass -noprofile -File "{0}" -installDir "{1}" -elevated' -f ($myinvocation.MyCommand.Definition),($pwd))
+    }
+    exit
+}
+
+$action = New-ScheduledTaskAction -Execute "regedit" -Argument "/s install.reg" -WorkingDirectory "$installDir"
 $trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Third-Party Flicker-Free Dimming HotKeys For MyASUS" -Description "This task runs a third-party program to control ASUS Flicker-Free Dimming with hot keys" -Settings $settings -Force
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "for-asus-bright-ctrl regedit" -Description "This task edits the registry to allow the third-party program to control ASUS Flicker-Free Dimming with hot keys" -Settings $settings -Force -RunLevel Highest
+Start-ScheduledTask -TaskName "for-asus-bright-ctrl regedit"
--- scripts/uninstall.ps1
+++ scripts/uninstall.ps1
@@ -1,1 +1,26 @@
-Unregister-ScheduledTask -TaskName "Third-Party Flicker-Free Dimming HotKeys For MyASUS"
+# Elevation code from https://superuser.com/a/532109
+
+param(
+    [switch]$elevated,
+    [string]$installDir
+)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        echo "Need admin rights to uninstall!"
+    } else {
+        Stop-ScheduledTask -TaskName "for-asus-bright-ctrl"
+        Unregister-ScheduledTask -TaskName "for-asus-bright-ctrl"
+
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-ExecutionPolicy Bypass -noprofile -File "{0}" -installDir "{1}" -elevated' -f ($myinvocation.MyCommand.Definition),($pwd))
+    }
+    exit
+}
+
+Unregister-ScheduledTask -TaskName "for-asus-bright-ctrl regedit"
+regedit /s "$installDir\uninstall.reg"
